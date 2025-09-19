9c2dcbffcf690a2ce55b3c7638f9bc328f58e86d
Mr.Scam <teamlikeminecraft@gmail.com>
2025-09-19 13:17:23 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -585,99 +585,30 @@ end
 Settings2 = ReadSetting2()
 if not game:GetService("Players").LocalPlayer.PlayerScripts.EffectsLocalThread.Disabled then game:GetService("Players").LocalPlayer.PlayerScripts.EffectsLocalThread.Disabled = true end
 if getgenv().BFSettings.FpsBooster then
-    spawn(function()
-        PlayerGui.Notifications.Enabled = false
-
-        L_1 = game:GetService("Workspace");
-        L_2 = game:GetService("Lighting");
-        L_3 = L_1.Terrain;
-        L_4 = game:GetService("Players");
-        L_5 = L_4.LocalPlayer.Character;
-
-        L_3.WaterWaveSize = 0; L_3.WaterWaveSpeed = 0; L_3.WaterReflectance = 0; L_3.WaterTransparency = 0;
-        L_2.GlobalShadows = false; L_2.FogEnd = tonumber(9e9); L_2.Brightness = 0;
-        settings().Rendering.QualityLevel = "Level01";
-        settings().Rendering.GraphicsMode = "NoGraphics";
-        UserSettings():GetService("UserGameSettings").MasterVolume = 0
-        UserSettings():GetService("UserGameSettings").SavedQualityLevel = 1
-        workspace.LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
-        game:GetService("Lighting").GlobalShadows = false
-        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-        settings().Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Level04
-        --sethiddenproperty(L_2, "Technology", "Compatibility");
-        for i, v in pairs(L_1:GetDescendants()) do
-            if v.ClassName == "Part" or v.ClassName == "SpawnLocation" or v.ClassName == "WedgePart" or v.ClassName == "Terrain" or v.ClassName == "MeshPart" then
-                v.Material = "Plastic"; v.Reflectance = 0; v.CastShadow = false;
-            elseif v.ClassName == "Decal" or v:IsA("Texture") then
-                v.Texture = 0; v.Transparency = 1;
-            elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-                v.LightInfluence = 0; v.Texture = 0; 
-            elseif v:IsA("Explosion") then
-                v.BlastPressure = 0; v.BlastRadius = 0;
-            elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-                v.Enabled = false;
-            elseif v:IsA("MeshPart") then
-                v.Material = "Plastic"; v.Reflectance = 0; v.TextureId = 0; v.CastShadow = false; v.RenderFidelity =
-                    Enum.RenderFidelity.Performance;
-            elseif v.ClassName == "SpecialMesh" then
-                v.TextureId = "rbxassetid://0";
-            elseif v.ClassName == "Shirt" or v.ClassName == "Pants" or v.ClassName == "Accessory" then
-                v:Destroy();
+    if game.PlaceId==2753915549 or 4442272183 or 7449423635 then if game:GetService("ReplicatedStorage").Effect.Container.Shared:FindFirstChild("AirDash")then game:GetService("ReplicatedStorage").Effect.Container.Shared.AirDash:Destroy()end;if game:GetService("ReplicatedStorage").Effect.Container.Shared:FindFirstChild("LightningTP")then game:GetService("ReplicatedStorage").Effect.Container.Shared.LightningTP:Destroy()end;if game:GetService("ReplicatedStorage").Effect.Container.Misc:FindFirstChild("Damage")then game:GetService("ReplicatedStorage").Effect.Container.Misc.Damage:Destroy()end;if game:GetService("ReplicatedStorage").Effect.Container.Misc:FindFirstChild("Confetti")then game:GetService("ReplicatedStorage").Effect.Container.Misc.Confetti:Destroy()end;if game:GetService("ReplicatedStorage").Effect.Container:FindFirstChild("LevelUp")then game:GetService("ReplicatedStorage").Effect.Container.LevelUp:Destroy()end end
+    settings().Rendering.QualityLevel = "Level01";
+    settings().Rendering.GraphicsMode = "NoGraphics";
+    UserSettings():GetService("UserGameSettings").MasterVolume = 0
+    UserSettings():GetService("UserGameSettings").SavedQualityLevel = 1
+    workspace.LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
+    game:GetService("Lighting").GlobalShadows = false
+    game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+    settings().Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Level04
+local function ducanhcc(v)
+    if v and not v.Name:find('Flower') and tostring(v.Parent.Name) ~= 'MysticIsland' then
+        pcall(function()
+            if v:IsA('Texture') and not v:GetAttribute('Offset') then
+                v:Destroy() 
+                return
             end
-        end
-        for i, v in pairs(L_2:GetDescendants()) do
-            if v:IsA("BlurEffect") or v:IsA("SunRaysEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("BloomEffect") or v:IsA("DepthOfFieldEffect") then
-                v.Enabled = false;
-            end
-        end
-        for i, v in pairs(L_5:GetDescendants()) do
-            if v.ClassName == "Shirt" or v.ClassName == "Pants" or v.ClassName == "Accessory" then
-                v:Destroy();
-            end
-        end
-
-        if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then     -- Blox Fruits
-            if game:GetService("ReplicatedStorage").Effect.Container.Shared:FindFirstChild("AirDash") then
-                game:GetService("ReplicatedStorage").Effect.Container.Shared.AirDash:Destroy();
-            end
-            if game:GetService("ReplicatedStorage").Effect.Container.Shared:FindFirstChild("LightningTP") then
-                game:GetService("ReplicatedStorage").Effect.Container.Shared.LightningTP:Destroy();
-            end
-            if game:GetService("ReplicatedStorage").Effect.Container.Misc:FindFirstChild("Damage") then
-                game:GetService("ReplicatedStorage").Effect.Container.Misc.Damage:Destroy();
-            end
-            if game:GetService("ReplicatedStorage").Effect.Container.Misc:FindFirstChild("Confetti") then
-                game:GetService("ReplicatedStorage").Effect.Container.Misc.Confetti:Destroy();
-            end
-            if game:GetService("ReplicatedStorage").Effect.Container:FindFirstChild("LevelUp") then
-                game:GetService("ReplicatedStorage").Effect.Container.LevelUp:Destroy();
-            end
-        end
-        shared.BC_2 = true
-        --[[for i, v in pairs(workspace.Map:GetDescendants()) do
-            if (i % 10000) == 0 then
-                task.wait()
-            end
-            pcall(function() v.Transparency = 1 end)
-            if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
-                v.Material = "Plastic"
-                v.Reflectance = 0
-            elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-                v.Transparency = 1
-            elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-                v.Lifetime = NumberRange.new(0)
-            elseif v:IsA("Explosion") then
-                v.BlastPressure = 1
-                v.BlastRadius = 1
-            elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-                v.Enabled = false
-            elseif v:IsA("MeshPart") then
-                v.Material = "Plastic"
-                v.Reflectance = 0
-                v.TextureID = 10385902758728957
-            end
-        end]] --
-    end)
+            v.Transparency = 1
+        end)
+    end
+end
+workspace.DescendantAdded:Connect(ducanhcc)
+for i, v in pairs(game.workspace:GetDescendants()) do
+    ducanhcc(v)
+    end
 end
 repeat wait() until type(CommF:InvokeServer("getInventory")) == "table"
 SUCCESS_FLAGS, COMBAT_REMOTE_THREAD = pcall(function()
@@ -1576,7 +1507,6 @@ local function Bring(Realmob, Enemy, BringCFrame)
     local count = tick()
     repeat
         --sizepart(Enemy)
-        if tick() - count >= 3.5 and Humanoid.Health == oldHealth then print(1) break end
         if not skidymf(Enemy) or not skidymf(Realmob) then return end
         if GetDistance(BringCFrame, RootPart.CFrame) <= 300 then
             if GetDistance(BringCFrame, RootPart.CFrame) > 5 or brought then
@@ -1622,7 +1552,6 @@ end
 function BringMob(Enemy, BringCFrame, notLimit)
     if not notLimit and #getHead() >= 2 then return end
     pcall(sethiddenproperty, LocalPlayer, "SimulationRadius", 5000)
-    local BringName     = Enemy.Name
     local EnemyRootPart = Enemy:FindFirstChild('HumanoidRootPart')
     if not EnemyRootPart then return end
     --if DetectPartMobBring(Enemy.Name, Enemy, false, MobSpawnClone[Enemy.Name]) then
@@ -1632,7 +1561,7 @@ function BringMob(Enemy, BringCFrame, notLimit)
     for i, v in pairs(workspace.Enemies:GetChildren()) do
         local dis = GetDistance(v.HumanoidRootPart.CFrame, BringCFrame)
         if not notLimit and #getHead() >= 2 then return end
-        if v and v.Name == Enemy.Name and skidymf(v) and isnetworkowner(v.HumanoidRootPart) and isnetworkowner2(v.HumanoidRootPart) and dis <= 300 and dis < min and not v:HasTag('realmob') and not v:HasTag("Brought") and not v:HasTag('ignore') then
+        if v and v.Name == BringName and skidymf(v) and isnetworkowner(v.HumanoidRootPart) and isnetworkowner2(v.HumanoidRootPart) and dis <= 300 and dis < min and not v:HasTag('realmob') and not v:HasTag("Brought") and not v:HasTag('ignore') then
             mob, min = v, dis
         end
         if mob then
@@ -1996,7 +1925,6 @@ function GetQuest(QuestTables, Boss)
         return
     end]]
     getgenv().CurrentFarmTaskDoing = 'Getting quest'
-    table.foreach(QuestTables, print)
     if not QuestTables or not QuestTables["Mob"] or not QuestTables["QuestName"] or not QuestTables["LevelReq"] or not QuestTables["QuestId"] or not QuestTables["QuestCFrame"] then
         if Boss then
             QuestTables = CheckBossQuest()
@@ -2056,7 +1984,6 @@ function CheckQuestByLevel(cq)
                     if v1.Task[Returner["Mob"]] then
                         for id, Data in next, v do
                             for nm, count in next, Data.Task do
-                                print(nm, CheckCurrentQuestMob())
                                 if tostring(nm) ~= CheckCurrentQuestMob() and count > 1 then
                                     if Data.LevelReq <= lvlPl then
                                         Returner["Mob"]       = tostring(nm)
@@ -2277,22 +2204,12 @@ function FarmMobByLevel(level)
                 KillNigga(CheckMob(CurrentQuestMob, true))
             else
                 getgenv().CurrentFarmTaskDoing = "Waiting For " .. tostring(CurrentQuestMob)
-                if #DetectPartSpawnMob(CurrentQuestMob) > 0 then
-                    local CFrameMob = DetectPartSpawnMob(CurrentQuestMob)
-                    for i,v in CFrameMob do
-                        TP1(v * CFrame.new(0,15,0))
-                        local count = tick()
-                        repeat wait() until tick() - count >= 0.5 or CheckMob(CurrentQuestMob)
-                        if CheckMob(CurrentQuestMob) then return end
-                    end
+                CFrame_Mob = CFrame_Mobs[CurrentQuestMob]
+                if type(CFrame_Mob) == "table" then
+                    TP1(CFrame_Mob[math.random(1, #CFrame_Mob)])
+                    wait(.25)
                 else
-                    CFrame_Mob = CFrame_Mobs[CurrentQuestMob]
-                    if type(CFrame_Mob) == "table" then
-                        TP1(CFrame_Mob[math.random(1, #CFrame_Mob)])
-                        wait(.25)
-                    else
-                        TP1(CFrame_Mob)
-                    end
+                    TP1(CFrame_Mob)
                 end
             end
         end
